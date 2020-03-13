8a96c4a3c736a907b221ac9cb2334a528fcf5cb7
davidmakowski <davidmakowski00@gmail.com>
2020-03-13 20:18:53 +0100

    player endpoint added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 from models import setup_db, Player, db
 
@@ -11,6 +12,18 @@ def create_app(test_config=None):
     def test_app():
         return jsonify({
             "greeting": "hallo mops"
+        })
+
+    @app.route('/players', methods=['GET'])
+    #@requires_auth('get:players')
+    def get_players(payload):
+        players = Player.query.all()
+
+        outplayers = [player.format() for player in players]
+
+        return jsonify({
+            'success': True,
+            'players': outplayers
         })
 
     return app
--- models.py
+++ models.py
@@ -18,3 +18,24 @@ class Player(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
     skill = Column(Integer)
+
+    def __init__(self, name, skill):
+        self.name = name
+        self.skill = skill
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def format(self):
+        return {
+            'name': self.name,
+            'skill': self.skill
+        }
